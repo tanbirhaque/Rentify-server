e0c03c14e731dcb30a3ab06a1e07667906e1616c
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-01-24 10:05:18 +0600

    property data get & request post

--- index.js
+++ index.js
@@ -29,12 +29,26 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const Propertycollection = client.db("RentifyDB").collection('Property')
+        const Requested_Propertiestcollection = client.db("RentifyDB").collection('Requested_Properties')
+
+        app.get('/property', async (req, res) => {
+            const result = await Propertycollection.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/requested-properties', async (req, res) => {
+            const propertyrequest = req.body;
+            const result = await Requested_Propertiestcollection.insertOne(propertyrequest)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
