19fc70bd033f2beeb387eb5f485a8a83afedbaa5
Tanbir Haque <tanbirhaque53@gmail.com>
2024-02-02 14:31:15 +0600

    Merging my branch

--- index.js
+++ index.js
@@ -110,12 +110,12 @@ async function run() {
 
 
 
-
+    // This API is for getting all the saved properties from the saved properties collection - Please don't remove the comment and the code below - By Tanbir
     //to save property data to backend
-    app.get("/saved-properties", async (req, res) => {
-      const result = await Saved_PropertiesCollection.find().toArray();
-      res.send(result);
-    });
+    // app.get("/saved-properties", async (req, res) => {
+    //   const result = await Saved_PropertiesCollection.find().toArray();
+    //   res.send(result);
+    // });
 
 
     app.post("/saved-properties", async (req, res) => {
@@ -126,13 +126,7 @@ async function run() {
       res.send(result);
     });
 
-    // app.get("/saved-properties", async (req, res) => {
-    //   const email = req.query.email;
-    //   const query = { email: email };
-    //   const result = await Saved_PropertiesCollection.find(query).toArray();
-    //   res.send(result);
-    // });
-
+    // This Api Is for getting saved properties for individual users. By Email query
     app.get("/saved-properties", async (req, res) => {
       try {
         const userEmail = req.query.email;
