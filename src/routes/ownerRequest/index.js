9c5b0bbe0a610c8d377891b017d216670ad3afeb
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-02-29 23:16:47 +0600

    owner singel data get

--- src/routes/ownerRequest/index.js
+++ src/routes/ownerRequest/index.js
@@ -1,9 +1,11 @@
 // this is api rout for payments authentication
 const ownerRequestInsert = require('../../api/ownerRequest/ownerRequestInsert');
+const ownerRequestedFind = require('../../api/ownerRequest/ownerRequestedFind');
 const ownerRequestedFindAll = require('../../api/ownerRequest/ownerRequestedFindAll');
 const router = require('express').Router();
 
 router.post("/ownerRequest", ownerRequestInsert)
 router.get("/ownerRequest", ownerRequestedFindAll)
+router.get("/ownerRequest/:email", ownerRequestedFind)
 
 module.exports = router
