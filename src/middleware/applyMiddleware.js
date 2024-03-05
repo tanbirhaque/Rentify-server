e8bc06d7fbc20429bb8b8bef462f8153be799e28
Tanbir Haque <tanbirhaque53@gmail.com>
2024-03-05 19:45:48 +0600

    Base url changed

--- src/middleware/applyMiddleware.js
+++ src/middleware/applyMiddleware.js
@@ -4,9 +4,11 @@ const cors = require('cors');
 const applyMiddleware = (app) => {
     app.use(cors({
         origin: [
+            // Live link for main branch only & localhost links for development branch. - Don't use live link while developing
             // 'https://rentify-70183.web.app',
+            // 'https://rentify-admin.firebaseapp.com',
             'http://localhost:5173',
-            'https://rentify-admin.firebaseapp.com',
+            'http://localhost:5174',
         ],
     }))
     app.use(express.json())
